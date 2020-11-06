ba1d2567542ee0a99fa6c641d8da79acade22da4
harshv2013 <harsh2013@gmail.com>
2020-11-06 13:15:14 +0530

    terraform updated

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -11,7 +11,7 @@ terraform {
   backend "s3" {
     bucket = "mybucket" # Will be overridden from build
     key    = "path/to/my/key" # Will be overridden from build
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
 
